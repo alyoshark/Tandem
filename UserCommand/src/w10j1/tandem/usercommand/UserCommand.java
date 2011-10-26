47ee0d4191c4e3bed280bd26b4bdf5327465f5d9
tienthanh8490 <tienthanh8490@gmail.com>
2011-10-27 05:48:46 +0800

    Update UserCommand/src/w10j1/tandem/usercommand/UserCommand.java

--- UserCommand/src/w10j1/tandem/usercommand/UserCommand.java
+++ UserCommand/src/w10j1/tandem/usercommand/UserCommand.java
@@ -1,106 +1,70 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-package w10j1.tandem.datakeeper;
+package w10j1.tandem.usercommand;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import static w10j1.tandem.datakeeper.TaskComparator.*;
-import w10j1.tandem.datakeeper.api.DataKeeper;
-import w10j1.tandem.task.api.Task;
+import w10j1.tandem.commandparser.api.CommandParser;
+import w10j1.tandem.commandprocessor.api.CommandProcessor;
+import w10j1.tandem.commandprocessor.CommandProcessorImpl;
+import w10j1.tandem.commandparser.CommandParserImpl;
 
 /**
  *
  * @author Chris
  */
-public class DataKeeperImpl implements DataKeeper {
+public class UserCommand {
 
-    public ArrayList<Task> taskList;
-    public ArrayList<Task> searchList;
+    public static CommandParser cpar = new CommandParserImpl();
+    public static CommandProcessor cpro = new CommandProcessorImpl();
+    public String request = "";
+    public String command = "";
+    private boolean isAfterSearch = false;
+    String executionResultStr;
 
-    public DataKeeperImpl() {
-        taskList = new ArrayList<Task>();
+    public UserCommand(String input) {
+        UserCommand.cpar.readRawInput(input);
+        UserCommand.cpar.setRequest();
+        this.request = UserCommand.cpar.getRequest();
+        this.command = UserCommand.cpar.getCommand();
     }
 
-    @Override
-    public String ascDue() {
-        Collections.sort(searchList, ascending(DUE_SORT));
-        return this.resultString();
+    public String getExecutionResults(){
+        return executionResultStr;
     }
-
-    @Override
-    public String decDue() {
-        Collections.sort(searchList, decending(DUE_SORT));
-        return this.resultString();
-    }
-
-    @Override
-    public String ascPriority() {
-        Collections.sort(searchList, ascending(PRIORITY_SORT));
-        return this.resultString();
-    }
-
-    @Override
-    public String decPriority() {
-        Collections.sort(searchList, decending(PRIORITY_SORT));
-        return this.resultString();
-    }
-
-    @Override
-    public String memToFile() {
-        Collections.sort(taskList, ascending(DUE_SORT));
-        StringBuilder sb = new StringBuilder();
-        for (Task t : taskList) {
-            sb.append(t.toString());
-        }
-        return sb.toString();
-    }
-
-    @Override
-    public ArrayList<Task> fileToMem(String fromFile) {
-        // TODO
-        // Create a new Calendar instance with the info provided and description string.
-        // Leave aside the done/undone and priority first.
-        // Suppose the record is still stored in the same way in side the data file as 0.1 version.
-        // dd|mm|yyyy|hh|mm|description..
-        throw new UnsupportedOperationException("Not supported yet.");
-    }
-
-    @Override
-    public String resultString() {
-        StringBuilder sb = new StringBuilder();
-        for (Task t : searchList) {
-            sb.append(t.getDesc()).append("\r\n");
-        }
-        return sb.toString();
-    }
-
-    @Override
-    public void addTask(Task task) {
-        taskList.add(task);
-    }
-
-    @Override
-    public void searchTask(String keywords) {
-        String[] kw = keywords.split("\\s");
-        for (Task task: taskList) {
-            boolean hasAllWords = true;
-            for (String word: kw) {
-                if (!task.getDesc().contains(word)) {
-                    hasAllWords = false;
-                    break;
+    
+    public void execute() {
+        switch (this.request.charAt(0)) {
+            case 'a':
+                this.cpro.add(this.cpar.getDue(), this.command);
+                executionResultStr = ""; //stub
+                break;
+            case 's':
+                this.cpro.search(this.command);
+                executionResultStr =""; //stub
+                break;
+            case 'u':
+                this.cpro.undo();
+                executionResultStr =""; //stub
+                break;
+            case 'e':
+            case 'r':
+            case 'd':
+                if (isAfterSearch) {
+                    if (this.request.charAt(0) == 'e') {
+                        this.cpro.edit(this.command);
+                        executionResultStr =""; //stub
+                    } else if (this.request.charAt(0) == 'r') {
+                        this.cpro.remove(this.command);
+                        executionResultStr = ""; //stub
+                    } else if (this.request.charAt(0) == 'd') {
+                        this.cpro.setDone(this.command);
+                        executionResultStr = ""; //stub
+                    }
+                } else {
+                    // System.out.println("Please perform a search first: s {keywords/date}");
+                    // Should be printed in the GUI not the console...
                 }
-            }
-            if (hasAllWords) {
-                searchList.add(task);
-            }
+                break;
+            default:
+                // Print an error message in the GUI.
+                // May be done by throwing an exception.
         }
-    }
-
-    @Override
-    public void removeTask(Task task) {
-        searchList.remove(task);
-        taskList.remove(task);
     }
 }
